fcc50ff82ea37882b1a7f615517ec6cb0a574df7
Travelinglight <3130000692@zju.edu.cn>
2015-05-25 11:20:08 +0800

    headHandler refined

--- G2/Proxy/HeaderHandler.js
+++ G2/Proxy/HeaderHandler.js
@@ -2,10 +2,9 @@ function getPath(header) {
   var Path = new Array();
   var wholePath = header.substring(header.indexOf('>') + 1);
   var chop = wholePath.split(',');
-  var regexp = /(WIDE.*|[Qq]AC|[Qq]AX|[Qq]AU|[Qq]Ao|[Qq]AO|[Qq]AS|[Qq]Ar|[Qq]AR|[Qq]AZ|[Qq]AI|TCPIP.*|TRACE.*|RELAY.*)/
+  var regexp = /(WIDE.*|[Qq]AC|[Qq]AX|[Qq]AU|[Qq]Ao|[Qq]AO|[Qq]AS|[Qq]Ar|[Qq]AR|[Qq]AZ|[Qq]AI|TCPIP.*|TRACE.*|RELAY.*|APRS.*)/
 
   for(var i = 1; i < chop.length; i++) {
-      if(!chop[i].match(/WIDE/)&&!chop[i].match(/qA/)&&!chop[i].match(/TCPIP/))
       if(!regexp.test(chop[i])) {
         var astic = chop[i].indexOf("*");
         if(astic==-1)
