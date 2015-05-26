e77a1082e6e938800b3ecd5602eded9ea051908a
luyor <807339657@qq.com>
2015-05-26 11:32:27 +0800

    fix insert format bug

--- G5/app.js
+++ G5/app.js
@@ -45,27 +45,27 @@ app.post('/weather', function(req, res) {
     //console.log(req.body)
     //console.log(req.body.data);
     ++received
-    var insert = 'insert into weather (Type,Month,Day,Hour,Min,Sec,Lat,Longi,WindDirection,WindSpeed,WeatherUnit,Gust,Temp,RainLastHr,RainLast24Hr,RainSinceMid,Humidity,Barometric,Luminosity,Path) values("' +
-        mysql.escape(req.body.Type) + '", "' +
-        mysql.escape(req.body.Month) + '", "' +
-        mysql.escape(req.body.Day) + '", "' +
-        mysql.escape(req.body.Hour) + '", "' +
-        mysql.escape(req.body.Min) + '", "' +
-        mysql.escape(req.body.Sec) + '", "' +
-        mysql.escape(req.body.Lat) + '", "' +
-        mysql.escape(req.body.Longi) + '", "' +
-        mysql.escape(req.body.WindDirection) + '", "' +
-        mysql.escape(req.body.WindSpeed) + '", "' +
-        mysql.escape(req.body.WeatherUnit) + '", "' +
-        mysql.escape(req.body.Gust) + '", "' +
-        mysql.escape(req.body.Temp) + '", "' +
-        mysql.escape(req.body.RainLastHr) + '", "' +
-        mysql.escape(req.body.RainLast24Hr) + '", "' +
-        mysql.escape(req.body.RainSinceMid) + '", "' +
-        mysql.escape(req.body.Humidity) + '", "' +
-        mysql.escape(req.body.Barometric) + '", "' +
-        mysql.escape(req.body.Luminosity) + '", "' +
-        mysql.escape(req.body.Path) + '")';
+    var insert = 'insert into weather (Type,Month,Day,Hour,Min,Sec,Lat,Longi,WindDirection,WindSpeed,WeatherUnit,Gust,Temp,RainLastHr,RainLast24Hr,RainSinceMid,Humidity,Barometric,Luminosity,Path) values(' +
+        mysql.escape(req.body.Type) + ', ' +
+        mysql.escape(req.body.Month) + ', ' +
+        mysql.escape(req.body.Day) + ', ' +
+        mysql.escape(req.body.Hour) + ', ' +
+        mysql.escape(req.body.Min) + ', ' +
+        mysql.escape(req.body.Sec) + ', ' +
+        mysql.escape(req.body.Lat) + ', ' +
+        mysql.escape(req.body.Longi) + ', ' +
+        mysql.escape(req.body.WindDirection) + ', ' +
+        mysql.escape(req.body.WindSpeed) + ', ' +
+        mysql.escape(req.body.WeatherUnit) + ', ' +
+        mysql.escape(req.body.Gust) + ', ' +
+        mysql.escape(req.body.Temp) + ', ' +
+        mysql.escape(req.body.RainLastHr) + ', ' +
+        mysql.escape(req.body.RainLast24Hr) + ', ' +
+        mysql.escape(req.body.RainSinceMid) + ', ' +
+        mysql.escape(req.body.Humidity) + ', ' +
+        mysql.escape(req.body.Barometric) + ', ' +
+        mysql.escape(req.body.Luminosity) + ', ' +
+        mysql.escape(req.body.Path) + ')';
     wsql.query(insert,function(err, rows, fields) {
             if (err) console.log(insert);
             else ++saved// console.log('weather_save_cnt:' + (++w_save_cnt));
@@ -77,15 +77,15 @@ app.post('/weather', function(req, res) {
 
 app.post('/moving_object', function(req, res) {
 	++received
-    var insert = 'insert into moving_object (Path,Source,Destination,Name,Time,Latitude,Longitude,Comment) values("' +
-        mysql.escape(req.body.Path) + '", "' +
-        mysql.escape(req.body.Source) + '", "' +
-        mysql.escape(req.body.Destination) + '", "' +
-        mysql.escape(req.body.Name) + '", "' +
-        mysql.escape(req.body.Time) + '", "' +
-        mysql.escape(req.body.Latitude) + '", "' +
-        mysql.escape(req.body.Longitude) + '", "' +
-        mysql.escape(req.body.Comment) + '")';
+    var insert = 'insert into moving_object (Path,Source,Destination,Name,Time,Latitude,Longitude,Comment) values(' +
+        mysql.escape(req.body.Path) + ', ' +
+        mysql.escape(req.body.Source) + ', ' +
+        mysql.escape(req.body.Destination) + ', ' +
+        mysql.escape(req.body.Name) + ', ' +
+        mysql.escape(req.body.Time) + ', ' +
+        mysql.escape(req.body.Latitude) + ', ' +
+        mysql.escape(req.body.Longitude) + ', ' +
+        mysql.escape(req.body.Comment) + ')';
 
     osql.query(insert, function(err, rows, fields) {
             if (err) console.log(insert);
