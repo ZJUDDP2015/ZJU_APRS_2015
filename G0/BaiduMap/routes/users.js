f58c9ebec90b2d7cf964eb7525dfc5e2ecbd4db3
Leeleo3x <leeleo3x@gmail.com>
2015-05-31 20:12:51 +0800

    integrate G7's code

--- G0/BaiduMap/routes/users.js
+++ G0/BaiduMap/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/user.js');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
     res.send('respond with a resource');
 });
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/changePassword', userController.changePassword);
+router.get('/logout', userController.logout);
+router.get('/get_position', userController.getSavedPosition, userController.getIPPosition);
+router.post('/post_position', userController.postPosition);
+router.get('/sessionVerify', userController.sessionVerify);
 
 module.exports = router;
