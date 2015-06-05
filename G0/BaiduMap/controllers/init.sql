54c118c4cec64dd2325a0f0b81cf231d9f0994be
Armour <497052684@qq.com>
2015-06-05 16:14:34 +0800

    changed init.sql (add user table)

--- G0/BaiduMap/controllers/init.sql
+++ G0/BaiduMap/controllers/init.sql
@@ -1,8 +1,9 @@
-create database if not exists aprs;
+CREATE DATABASE IF NOT EXISTS aprs;
 
-use aprs;
-create table if not exists weather (
-	id int not null AUTO_INCREMENT,
+USE aprs;
+
+CREATE TABLE IF NOT EXISTS weather (
+	id int NOT NULL AUTO_INCREMENT,
 	Type int,
 	Month int,
 	Day int,
@@ -21,13 +22,13 @@ create table if not exists weather (
 	RainSinceMid int,
 	Humidity int,
 	Barometric int,
-	Luminosity int,
+	LUMINOSITY INT,
 	Path varchar(100),
-	primary key(id)
-	);
+	PRIMARY KEY(id)
+);
 
-create table if not exists moving_object (
-	id int not null AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS moving_object (
+	id int NOT NULL AUTO_INCREMENT,
 	Path varchar(100),
 	Source varchar(12),
 	Destination varchar(12),
@@ -36,5 +37,19 @@ create table if not exists moving_object (
 	Latitude float,
 	Longitude float,
 	Comment varchar(255),
-	primary key(id)
-	);
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE `user` (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(30) DEFAULT NULL,
+  password varchar(20) DEFAULT NULL,
+  mapCenterX varchar(20) DEFAULT NULL,
+  mapCenterY varchar(20) DEFAULT NULL,
+  bannerImage varchar(50) DEFAULT NULL,
+  bannerWord varchar(50) DEFAULT NULL,
+  bannerWordSize varchar(20) DEFAULT NULL,
+  bannerWordColor varchar(20) DEFAULT NULL,
+  bannerWordFont varchar(20) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
