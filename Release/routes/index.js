93427f67dfbd18c2f049207b3ba9f693a2a547aa
Haaaaappy <573123470@qq.com>
2015-07-01 21:31:32 +0800

    sixty minutes data on map

--- Release/routes/index.js
+++ Release/routes/index.js
@@ -3,10 +3,37 @@ var router = express.Router();
 var userController = require('../controllers/user.js');
 var indexController = require('../controllers/index.js');
 
+var getFormatedTime = function(month, day, hour, minute) {
+    var stime = "2015";
+    if (hour < 0) {
+        hour += 24;
+        day--;
+    }
+    stime += '-' + (month < 10 ? '0' + month : month);
+    stime += '-' + (day < 10 ? '0' + day : day);
+    stime += ' ' + (hour < 10 ? '0' + hour : hour);
+    stime += ':' + (minute < 10 ? '0' + minute : minute) + ':00';
+    return stime;
+}
+
 router.get('/', function(req, res) {
     res.render('index', {
-        title: 'Index'
+        title: 'Index',
+        starttime: '',
+        endtime: '',
+        callsign: ''
     })
+});
+
+router.get('/callsn', function(req, res) {
+    var callsign_name = req.query.name;
+  //indexController.createCallsignEjs(callsign, res);
+    res.render('index',{
+        starttime: getFormatedTime((new Date()).getMonth() + 1, (new Date()).getDate(), (new Date()).getHours()-1, (new Date()).getMinutes()),//初始化时间(开始时间为当前一天前)
+        endtime: getFormatedTime((new Date()).getMonth() + 1, (new Date()).getDate(), (new Date()).getHours(), (new Date()).getMinutes()),
+        callsign: callsign_name
+    })
+    console.log(starttime);
 });
 
 router.get('/rowData60', function(req, res) {
