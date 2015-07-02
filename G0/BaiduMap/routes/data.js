4b6bc1264f52e3d051f02cd3c69a446c422310e3
kingston <3130000692@zju.edu.cn>
2015-07-02 08:45:00 +0800

    repo cleaned up

--- G0/BaiduMap/routes/data.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var dataController = require('../controllers/data.js');
-
-router.post('/weather', dataController.weather);
-router.post('/moving_object', dataController.moving_object);
-
-module.exports = router;
--- G1/CallSignLocator/README.md
+++
@@ -1,5 +0,0 @@
-# Syrah_CallSignLocator
-
-* Phantomjs is used to load the page rescourse
-* Maybe slow because simply used over 100 childProcess to load over 100 pages that needs to be search
-* Every search may take no more than 2 mins
